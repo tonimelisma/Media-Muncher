549fc2b780f7d4fc7a34cf319f8471ed8fc5a6cf
Toni Melisma <59022391+tonimelisma@users.noreply.github.com>
2024-09-28 19:34:32 -0700

    Initial Commit

---
+++ Media Muncher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Media Muncher
+//
+//  Created by Toni Melisma on 9/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
